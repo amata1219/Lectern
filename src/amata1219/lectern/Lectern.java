b1f7885b5ef3998edf883b17feaa5845f22bb7de
amata1219 <suzukiamata@gmail.com>
2020-05-11 20:21:00 +0900

    こみっとー

--- src/amata1219/lectern/Lectern.java
+++ src/amata1219/lectern/Lectern.java
@@ -9,8 +9,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
-import org.bukkit.block.Chest;
+import org.bukkit.block.Container;
 import org.bukkit.block.Sign;
+import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -24,8 +25,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
@@ -33,10 +32,8 @@ public class Lectern extends JavaPlugin implements Listener {
 
 	private static Lectern plugin;
 	private static final String SIGN_TITLE = "[Lectern]";
-	private static final ItemStack glass = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
-	private static Constructor<?> newMinecraftKey;
-	private static Constructor<?> newPacketDataSerializer;
-	private static Constructor<?> newPacketPlayOutCustomPayload;
+	private static final ItemStack BACKGROUND = new ItemStack(Material.LIGHT_GRAY_STAINED_GLASS_PANE);
+	private static Object packetPlayOutOpenBook;
 	private static Method getHandle;
 	private static Field playerConnection;
 	private static Method sendPacket;
@@ -44,32 +41,40 @@ public class Lectern extends JavaPlugin implements Listener {
 	static{
 		String version = Bukkit.getServer().getClass().getPackage().getName().replaceFirst(".*(\\d+_\\d+_R\\d+).*", "$1");
 		String nms = "net.minecraft.server.v" + version;
-		Constructor<?> arg0 = null, arg1 = null, arg2 = null;
+		Object arg0 = null;
+		Method arg1 = null;
+		Field arg2 = null;
 		Method arg3 = null;
-		Field arg4 = null;
-		Method arg5 = null;
 		try{
-			Class<?> MinecraftKey = Class.forName(nms + ".MinecraftKey");
-			arg0 = MinecraftKey.getConstructor(String.class);
-			Class<?> PacketDataSerializer = Class.forName(nms + ".PacketDataSerializer");
-			arg1 = PacketDataSerializer.getConstructor(ByteBuf.class);
-			arg2 = Class.forName(nms + ".PacketPlayOutCustomPayload").getConstructor(MinecraftKey, PacketDataSerializer);
+			Class<?> EnumHand = Class.forName(nms + ".EnumHand");
+			Object MAIN_HAND = null;
+			for(Object value : EnumHand.getEnumConstants()){
+				if(value.toString().equals("MAIN_HAND")){
+					MAIN_HAND = value;
+					break;
+				}
+			}
+			
+			Class<?> PacketPlayOutOpenBook = Class.forName(nms + ".PacketPlayOutOpenBook");
+			Constructor<?> constructor = PacketPlayOutOpenBook.getConstructor(EnumHand);
+			
+			arg0 = constructor.newInstance(MAIN_HAND);
+			
 			Class<?> CraftPlayer = Class.forName("org.bukkit.craftbukkit.v" + version + ".entity.CraftPlayer");
-			arg3 = CraftPlayer.getMethod("getHandle");
+			arg1 = CraftPlayer.getMethod("getHandle");
 			Class<?> EntityPlayer = Class.forName(nms + ".EntityPlayer");
-			arg4 = EntityPlayer.getDeclaredField("playerConnection");
-			arg4.setAccessible(true);
-			Class<?> PlayerConnection = Class.forName(nms + ".PlayerConnection");//PlayerConnection
-			arg5 = PlayerConnection.getMethod("sendPacket", Class.forName(nms + ".Packet"));
+			arg2 = EntityPlayer.getDeclaredField("playerConnection");
+			arg2.setAccessible(true);
+			Class<?> PlayerConnection = Class.forName(nms + ".PlayerConnection");
+			arg3 = PlayerConnection.getMethod("sendPacket", Class.forName(nms + ".Packet"));
 		}catch(Exception e){
 			e.printStackTrace();
 		}
-		newMinecraftKey = arg0;
-		newPacketDataSerializer = arg1;
-		newPacketPlayOutCustomPayload = arg2;
-		getHandle = arg3;
-		playerConnection = arg4;
-		sendPacket = arg5;
+		
+		packetPlayOutOpenBook = arg0;
+		getHandle = arg1;
+		playerConnection = arg2;
+		sendPacket = arg3;
 	}
 
 	@Override
@@ -94,8 +99,7 @@ public class Lectern extends JavaPlugin implements Listener {
 		if(player.hasPermission("lectern.create"))
 			return;
 
-		if(!SIGN_TITLE.equals(e.getLine(0)))
-			return;
+		if(!SIGN_TITLE.equals(e.getLine(0))) return;
 
 		e.setCancelled(true);
 		sendMessage(player, ChatColor.RED + (isJapanese(player) ? "あなたには書見台を作成する権限がありません。" : "You don't have permission to create lectern."));
@@ -103,38 +107,32 @@ public class Lectern extends JavaPlugin implements Listener {
 
 	@EventHandler(ignoreCancelled = true)
 	public void onInteract(PlayerInteractEvent e){
-		if(e.getAction() != Action.RIGHT_CLICK_BLOCK)
-			return;
+		if(e.getAction() != Action.RIGHT_CLICK_BLOCK) return;
 
 		Block block = e.getClickedBlock();
 		Material material = block.getType();
-		if(material != Material.ENCHANTING_TABLE)
-			return;
+		if(material != Material.ENCHANTING_TABLE) return;
 
 		Block down = block.getRelative(BlockFace.DOWN);
-		if(down == null || down.getType() != Material.CHEST)
-			return;
+		Material type = down.getType();
+		if(down == null || type != Material.CHEST && type != Material.TRAPPED_CHEST && type != Material.BARREL) return;
 
-		Chest chest = (Chest) down.getState();
+		//https://www.spigotmc.org/threads/set-sign-texts-use-blockdata-api.370354/
 		boolean find = false;
 		for(BlockFace face : BlockFace.values()){
 			Block relative = block.getRelative(face);
-			if(relative == null || relative.getType() != Material.WALL_SIGN)
-				continue;
+			if(relative == null || !relative.getType().toString().endsWith("_WALL_SIGN")) continue;
+
+			if(face != ((Directional) relative.getBlockData()).getFacing()) continue;
 
 			Sign sign = (Sign) relative.getState();
-			if(face != ((org.bukkit.material.Sign) sign.getData()).getFacing())
-				continue;
-
-			if(!SIGN_TITLE.equals(sign.getLine(0)))
-				continue;
-
-			find = true;
-			break;
+			if(SIGN_TITLE.equals(sign.getLine(0))){
+				find = true;
+				break;
+			}
 		}
-
-		if(!find)
-			return;
+		
+		if(!find) return;
 
 		Player player = e.getPlayer();
 		if(!player.hasPermission("lectern.use")){
@@ -144,12 +142,12 @@ public class Lectern extends JavaPlugin implements Listener {
 		}
 
 		Inventory inventory = Bukkit.createInventory(null, 27, "§8Lectern");
-		for(ItemStack item : chest.getBlockInventory().getContents())
+		for(ItemStack item : ((Container) down.getState()).getInventory().getContents())
 			if(item != null && item.getType() == Material.WRITTEN_BOOK)
 				inventory.setItem(inventory.firstEmpty(), item);
 
 		while(inventory.firstEmpty() != -1)
-			inventory.setItem(inventory.firstEmpty(), glass);
+			inventory.setItem(inventory.firstEmpty(), BACKGROUND);
 
 		e.setCancelled(true);
 		player.openInventory(inventory);
@@ -157,30 +155,34 @@ public class Lectern extends JavaPlugin implements Listener {
 
 	@EventHandler(ignoreCancelled = true)
 	public void onClick(InventoryClickEvent e){
-		Inventory inventory = e.getClickedInventory();
-		if(inventory == null)
-			return;
-
-		String title = inventory.getTitle();
-		if(title == null || !title.equals("§8Lectern"))
-			return;
+		String title = e.getView().getTitle();
+		if(title == null || !title.equals("§8Lectern")) return;
 
 		e.setCancelled(true);
 
 		ItemStack item = e.getCurrentItem();
-		if(item == null || item.getType() != Material.WRITTEN_BOOK)
-			return;
+		if(item == null || item.getType() != Material.WRITTEN_BOOK) return;
 
-		if(!(e.getWhoClicked() instanceof Player))
-			return;
+		if(!(e.getWhoClicked() instanceof Player)) return;
 
 		Player player = (Player) e.getWhoClicked();
-		PlayerInventory playerInventory = player.getInventory();
-		ItemStack main = playerInventory.getItemInMainHand();
-		playerInventory.setItemInMainHand(item);
-		ByteBuf buffer = Unpooled.buffer(256);
+		PlayerInventory inventory = player.getInventory();
+		ItemStack main = inventory.getItemInMainHand();
+		
+		try{
+			inventory.setItemInMainHand(item);
+			Object entity = getHandle.invoke(player);
+			Object connection = playerConnection.get(entity);
+			sendPacket.invoke(connection, packetPlayOutOpenBook);
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}finally{
+			inventory.setItemInMainHand(main);
+		}
+		/*ByteBuf buffer = Unpooled.buffer(256);
 		buffer.setByte(0, (byte) 0);
 		buffer.writerIndex(1);
+		
 		try{
 			Object data = newPacketDataSerializer.newInstance(buffer);
 			Object key = newMinecraftKey.newInstance("minecraft:book_open");
@@ -188,11 +190,12 @@ public class Lectern extends JavaPlugin implements Listener {
 			Object entity = getHandle.invoke(player);
 			Object connection = playerConnection.get(entity);
 			sendPacket.invoke(connection, packet);
+			System.out.println("sent");
 		}catch(Exception ex){
 			ex.printStackTrace();
 		}finally{
 			playerInventory.setItemInMainHand(main);
-		}
+		}*/
 	}
 
 	public boolean isJapanese(Player player){
